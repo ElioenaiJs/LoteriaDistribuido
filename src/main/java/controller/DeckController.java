f99992c53150dd9b5b14643f6b67f847cdbfe2ad
Sainux <sainusbaldo08@gmail.com>
2024-04-30 18:17:46 -0600

    show random images in view

--- src/main/java/controller/DeckController.java
+++ src/main/java/controller/DeckController.java
@@ -1,27 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import java.util.Collections;
+import java.util.List;
+import model.Card;
 import model.Deck;
 import service.DeckService;
 
-/**
- *
- * @author froste
- */
-@AllArgsConstructor
-@NoArgsConstructor
 public class DeckController {
    
     private DeckService deckService;
     
-    public Deck getDeck(){
+    public DeckController() {
         deckService = new DeckService();
+    }
+    
+    public Deck getDeck() {
         return deckService.getDeck();
     }
     
+    public List<Card> getShuffledDeck() {
+        Deck deck = deckService.getDeck();
+        deck.shuffle(); // Barajar las cartas en el mazo
+        return deck.getCards(); // Devolver las cartas barajadas
+    }
 }
